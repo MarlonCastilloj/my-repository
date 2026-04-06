dec96c176bc1f61be86e3dba05139bc33ffa3aab
MarlonCastilloj <marlon04jose.gmail.com>
2026-04-05 19:02:23 -0500

    menu corregido y funcion menu en services corregida

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 from src.services import *
-program = False
 
-while program == False:
+while True:
     option = menu_principal()
-    if option >= 1 & option <=8:
+
+    if option >= 1 and option <= 8:
         if option == 1:
             create_product()
+
         elif option == 2: 
             show_product()
+
         elif option == 3: 
             search_product()
+
         elif option == 4:  
             update_product()
+
         elif option == 5:
             delete_product()
+
         elif option == 6:
             statistics_product()
+
         elif option == 7:
             save_csv()
+
         elif option == 8:
             upload_csv()
-        elif option == 9: break
-    
+
+    elif option == 9:
+        print("__El programa finalizó, hasta luego__")
+        break
+
+    else:
+        print("Opción inválida ❌ \nVuelva a digitar una opción válida")
